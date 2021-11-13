2c635c671159098feffe646c53c4351e0a8b7fbb
CGOD737 <59667212+CGOD737@users.noreply.github.com>
2021-11-13 18:35:29 -0500

    Test Case
    Using BestBuy.com as a test.

--- BotServer.py
+++ BotServer.py
@@ -1,33 +1,39 @@
 #Server to Run WebBot and to make for easy user input.
-
+#Flask Imports
 import flask
 from flask import request
 from flask.templating import render_template
 
+import WebBot
 app = flask.Flask(__name__)
+
+#Global Variables
 
 #Default Route
 @app.route('/')
 def home():
     return render_template('index.html')
 
+#Gets the Input Values and makes the call to run the bot
 @app.route('/submitted', methods=['GET', 'POST'])
 def submitted():
-    fname=request.form['fname']
-    lname=request.form['lname']
-    email=request.form['email']
 
-    print(fname)
+    runBot(request.form['fname'],request.form['lname'],request.form['email'], request.form['password'])
 
     return render_template('submitted.html')
 
-    
+def runBot(fname, lname, email, password):
 
-#Runs WebBot through a single cycle.
-#@app.route('/run', methods=['POST'])
+    #instantiates the object
+    bot = WebBot.WebBot()
 
-#Ends WebBot Cycle
-#@app.route('/end', methods=['POST'])
+    bot.login(email, password)
 
+    print("Bot is now Running....")
+
+
+    return
+
+#default method
 if __name__ == '__main__':
     app.run(debug=True)
--- WebBot.py
+++ WebBot.py
@@ -2,31 +2,37 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+#Assuming chrome driver is in this location, change for whatever path you may have
+PATH = "C:\Program Files (x86)\chromedriver.exe"
 
 #Temporarily stores login information to use later on in the class.
 
 #Actual Class
 class WebBot:
     def __init__(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get("site.com")
-
+        print("WebBot is now intiating...")
+        self.driver = webdriver.Chrome(PATH)
+        
     def accept_cookies(self):
         button = self.driver.find_element_by_id("privacy-layer-accept-all-button")
         button.click()
 
     def login(self, username, password):
-        self.driver.get("site.com")
-        email_input = self.driver.find_element_by_id("mms-login-form__email")
+        self.driver.get("https://www.bestbuy.com/identity/signin?token=tid%3A47187327-44d5-11ec-8152-12a67a811cc3")
+        email_input = self.driver.find_element_by_id("fld-e")
         email_input.clear()
         email_input.send_keys(username)
-        pass_input = self.driver.find_element_by_id("mms-login-form__password")
+        pass_input = self.driver.find_element_by_id("fld-p1")
         pass_input.clear()
         pass_input.send_keys(password)
-        self.driver.find_element_by_id("mms-login-form__login-button").click()
+        time.sleep(10)
 
 
 if __name__ == '__main__':
+
     WebBot = WebBot()
 
     account = "username"
