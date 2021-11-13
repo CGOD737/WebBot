d0d1bd093dbd245082f781136b4bae891e625e81
CGOD737 <59667212+CGOD737@users.noreply.github.com>
2021-11-13 16:38:55 -0500

    Reorganized Repository

--- BotServer.py
+++ BotServer.py
@@ -1,6 +1,6 @@
 #Server to Run WebBot and to make for easy user input.
 
-from Flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 #Has you enter your login information for the WebBot. Gets the actual information and saves it as a string for the webbot.
@@ -12,5 +12,14 @@ app = Flask(__name__)
 #Ends WebBot Cycle
 @app.route('/end', methods=['POST'])
 
-if __name__ == '__main__'
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
     app.run(host='0.0.0.0', port=port)
+
+
